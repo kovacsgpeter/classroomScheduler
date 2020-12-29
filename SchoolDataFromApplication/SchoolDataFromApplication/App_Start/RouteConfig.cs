2e92999d116e5c926a50668940241dbf727093cc
abel.bahor <0525.b.abel@gmail.com>
2020-12-29 19:01:10 +0100

    SchoolController

--- SchoolDataFromApplication/SchoolDataFromApplication/App_Start/RouteConfig.cs
+++ SchoolDataFromApplication/SchoolDataFromApplication/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SchoolDataFromApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "School", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SchoolDataFromApplication/SchoolDataFromApplication/Controllers/SchoolController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolDataFromApplication.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: School
+        public ActionResult School()
+        {
+            return View();
+        }
+    }
+}
